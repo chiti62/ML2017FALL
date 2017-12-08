14cd8d389b77c748cb2513ed424e1b1c87afbc26
Chinkai Ting <b02611012@ntu.edu.tw>
2017-12-08 22:46:17 +0800

    v3

--- hw4/hw4_predict.sh
+++ hw4/hw4_predict.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-
+wget -O hw4_model.h5 'https://www.dropbox.com/s/1pihyrdm4ja1fpw/hw4_model.h5?dl=1'
 python3 hw4_predict.py $@ $@
--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-wget -O hw4_model.h5 'https://www.dropbox.com/s/1pihyrdm4ja1fpw/hw4_model.h5?dl=1'
 python3 hw4_train.py $@ $@
